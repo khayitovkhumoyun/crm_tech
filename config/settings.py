489a482362eede10969642ba8503e23a11e0a832
admin <khayitovkhumoyun000@gmail.com>
2024-06-14 11:37:23 +0500

    rol

--- config/settings.py
+++ config/settings.py
@@ -142,20 +142,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'todoApp',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'todoApp',
+#
+#     }
+# }
 
 #
 # DATABASES = {
